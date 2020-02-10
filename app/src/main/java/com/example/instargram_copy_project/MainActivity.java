d337c11393d9911fde41661366d4a2539ca94783
yoonho0922 <ahn3330@naver.com>
2020-02-10 22:02:02 +0900

    기본 틀 정리

--- app/src/main/java/com/example/instargram_copy_project/MainActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/MainActivity.java
@@ -2,84 +2,67 @@ package com.example.instargram_copy_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button home_btn;
+    Button search_btn;
+    Button posting_btn;
+    Button notification_btn;
+    Button my_page_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sunjung_main2);
+        setContentView(R.layout.activity_main);
 
-        final ToggleButton toggle1 = findViewById(R.id.toggleButton1);
-        toggle1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        home_btn = findViewById(R.id.home_btn);
+        search_btn = findViewById(R.id.search_btn);
+        posting_btn = findViewById(R.id.posting_btn);
+        notification_btn = findViewById(R.id.notification_btn);
+        my_page_btn = findViewById(R.id.my_page_btn);
+
+        home_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(toggle1.isChecked()) {
-                    toggle1.setBackgroundDrawable(getResources().getDrawable(R.drawable.homebutton));
-                }
-                else{
-                    toggle1.setBackgroundDrawable(getResources().getDrawable(R.drawable.homeonbutton));
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
             }
         });
-
-        final ToggleButton toggle2 = findViewById(R.id.toggleButton2);
-        toggle2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        search_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(toggle2.isChecked()) {
-                    toggle2.setBackgroundDrawable(getResources().getDrawable(R.drawable.searchbutton));
-                }
-                else{
-                    toggle2.setBackgroundDrawable(getResources().getDrawable(R.drawable.searchonbutton));
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
             }
         });
-
-
-        final ToggleButton toggle3 = findViewById(R.id.toggleButton3);
-        toggle3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        posting_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(toggle3.isChecked()) {
-                    toggle3.setBackgroundDrawable(getResources().getDrawable(R.drawable.addbutton));
-                }
-                else{
-                    toggle3.setBackgroundDrawable(getResources().getDrawable(R.drawable.addonbutton));
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PostingActivity.class);
+                startActivity(intent);
             }
         });
-
-
-        final ToggleButton toggle4 = findViewById(R.id.toggleButton4);
-        toggle4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        notification_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(toggle4.isChecked()) {
-                    toggle4.setBackgroundDrawable(getResources().getDrawable(R.drawable.heartbutton));
-                }
-                else{
-                    toggle4.setBackgroundDrawable(getResources().getDrawable(R.drawable.heartonbutton));
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                startActivity(intent);
             }
         });
-
-
-        final ToggleButton toggle5 = findViewById(R.id.toggleButton5);
-        toggle5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        my_page_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(toggle5.isChecked()) {
-                    toggle5.setBackgroundDrawable(getResources().getDrawable(R.drawable.userbutton));
-                }
-                else{
-                    toggle5.setBackgroundDrawable(getResources().getDrawable(R.drawable.useronbutton));
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyPageActivity.class);
+                startActivity(intent);
             }
         });
 
     }
+
+
 }
--- app/src/main/java/com/example/instargram_copy_project/NotificationActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/NotificationActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Posting extends AppCompatActivity {
+public class NotificationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_posting);
+        setContentView(R.layout.activity_notification);
     }
 }
