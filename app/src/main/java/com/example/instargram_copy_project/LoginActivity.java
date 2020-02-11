21cd5b8f816669cc9689edebda1297cbed69530b
KkiYya <sja3410@gmail.com>
2020-02-11 15:40:51 +0900

    로그인 기능

--- app/src/main/java/com/example/instargram_copy_project/LoginActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/LoginActivity.java
@@ -1,4 +1,86 @@
 package com.example.instargram_copy_project;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG ="LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.login_button2).setOnClickListener(onClickListener);
+        findViewById(R.id.resister_button).setOnClickListener(onClickListener);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) { //클릭된 위젯의 id를 이용
+            switch (v.getId()) {
+                case R.id.login_button2:
+                    Login();
+                    break;
+                case R.id.resister_button:
+                    startSignUpActivity();
+                    break;
+            }
+        }
+    };
+
+    public void Login(){
+        String email = ((EditText)findViewById(R.id.idlogin)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordlogin)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startToast("로그인 성공");
+                            startMainActivity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            startToast("로그인 실패");
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    private void startToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+    private void startSignUpActivity(){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+    private void startMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/instargram_copy_project/SignupActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/SignupActivity.java
@@ -1,4 +1,93 @@
 package com.example.instargram_copy_project;
 
-public class SignupActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "SignUpActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.signup_button).setOnClickListener(onClickListener);
+        findViewById(R.id.login_button1).setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) { //클릭된 위젯의 id를 이용
+            switch (v.getId()) {
+                case R.id.signup_button: //case문은 ':'표시
+                    //회원가임 버튼을 눌렀을때 회원가입 함수 실행
+                    signup();
+                    break;
+                case R.id.login_button1:
+                    startLoginActivity();
+                    break;
+
+            }
+
+        }
+    };
+
+    public void signup() { //회원가입 함수
+        String email = ((EditText) findViewById(R.id.idsignup)).getText().toString(); //email과 password를 입력한 값을 받아와 변수에 저장
+        String password = ((EditText) findViewById(R.id.passwordsignup)).getText().toString();
+        String re_password = ((EditText) findViewById(R.id.repasswordsignup)).getText().toString();
+        if (password.equals(re_password)) {
+            mAuth.createUserWithEmailAndPassword(email, password) //변수 email과 password의 저장된 값을 전송
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            }
+
+                            // ...
+                        }
+                    });
+            startToast("정상적으로 회원가입 되었습니다.");
+        } else {
+            startToast("비밀번호가 같지 않습니다.");
+        }
+    }
+
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
+
