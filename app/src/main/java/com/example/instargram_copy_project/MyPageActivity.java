8a1265b8d6eb10bb0592e9461792e62a1a6bf8d9
Rudy-009 <rofxnaos@gmail.com>
2020-02-13 17:47:21 +0900

    OnBottun

--- app/src/main/java/com/example/instargram_copy_project/MyPageActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/MyPageActivity.java
@@ -2,13 +2,30 @@ package com.example.instargram_copy_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyPageActivity extends AppCompatActivity {
+
+    Button toggleButton4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+
+        toggleButton4 = findViewById(R.id.toggleButton4);
+
+        toggleButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyPageActivity.this , NotificationActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/instargram_copy_project/NotificationActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/NotificationActivity.java
@@ -2,13 +2,31 @@ package com.example.instargram_copy_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    Button toggleButton5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        toggleButton5 = findViewById(R.id.toggleButton5);
+
+        toggleButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotificationActivity.this , MyPageActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+
+            }
+        });
+
     }
 }
