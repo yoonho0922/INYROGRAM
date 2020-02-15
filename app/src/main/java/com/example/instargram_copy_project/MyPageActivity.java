ae53e26d3a860a391ab32f0c4cb12ced73a94837
jiminAN <als398@naver.com>
2020-02-15 13:01:27 +0900

    화면전환 애니메이션 xml추가

--- app/src/main/java/com/example/instargram_copy_project/MyPageActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/MyPageActivity.java
@@ -1,15 +1,17 @@
 package com.example.instargram_copy_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MyPageActivity extends AppCompatActivity {
 
     Button toggleButton4;
+    Button profileEditBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class MyPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_page);
 
         toggleButton4 = findViewById(R.id.toggleButton4);
+        profileEditBtn = findViewById(R.id.profileEditBtn);
 
         toggleButton4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +27,15 @@ public class MyPageActivity extends AppCompatActivity {
                 Intent intent = new Intent(MyPageActivity.this , NotificationActivity.class);
                 startActivity(intent);
                 overridePendingTransition(0, 0);
+
+            }
+        });
+        profileEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyPageActivity.this , ProfileEditActivity.class);
+                startActivity(intent);
+
 
             }
         });
--- app/src/main/java/com/example/instargram_copy_project/ProfileEditActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/ProfileEditActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ProfileEdit extends AppCompatActivity {
+public class ProfileEditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
