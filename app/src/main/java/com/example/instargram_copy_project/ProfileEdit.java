e95b10c29ed7f50dd2ce5a535b408b0c5311a261
jiminAN <als398@naver.com>
2020-02-15 12:50:48 +0900

    프로필 수정 엑티비티 추가 :manifest 변경

---
+++ app/src/main/java/com/example/instargram_copy_project/ProfileEdit.java
@@ -0,0 +1,14 @@
+package com.example.instargram_copy_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_edit);
+    }
+}
