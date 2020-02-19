a659c7e8ed305f23f26127a2422490ed9ba7023c
yoonho0922 <ahn3330@naver.com>
2020-02-19 13:00:48 +0900

    postingText 페이지 추가

---
+++ app/src/main/java/com/example/instargram_copy_project/PostingTextActivity.java
@@ -0,0 +1,14 @@
+package com.example.instargram_copy_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostingTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posting_text);
+    }
+}
