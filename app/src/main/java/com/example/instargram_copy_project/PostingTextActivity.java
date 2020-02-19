ef2899529707305b4da994c4ea2adf49c5424a64
yoonho0922 <ahn3330@naver.com>
2020-02-19 13:11:12 +0900

    postingText 페이지 추가

--- app/src/main/java/com/example/instargram_copy_project/PostingTextActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/PostingTextActivity.java
@@ -3,8 +3,12 @@ package com.example.instargram_copy_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PostingTextActivity extends AppCompatActivity {
+    EditText content;
+    Button posting_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
