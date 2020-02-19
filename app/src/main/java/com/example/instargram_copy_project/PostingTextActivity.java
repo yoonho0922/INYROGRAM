d8c4a9a212912c7c0d3efb3dc91b3f243aa1ecac
yoonho0922 <ahn3330@naver.com>
2020-02-19 13:48:50 +0900

    포스팅테스트 기능 구현

--- app/src/main/java/com/example/instargram_copy_project/PostingTextActivity.java
+++ app/src/main/java/com/example/instargram_copy_project/PostingTextActivity.java
@@ -1,18 +1,85 @@
 package com.example.instargram_copy_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostingTextActivity extends AppCompatActivity {
+
+    private static final String TAG = "MemberInfoSetting";
+
     EditText content;
     Button posting_btn;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); //user의 정보를 사용할것임
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef = db.collection("Profile").document(user.getUid());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting_text);
+
+        posting_btn = findViewById(R.id.posting_btn);
+
+        posting_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                posting();
+            }
+        });
     }
+
+    public void posting(){
+        String userUid = user.getUid(); //유저 UID 가져오기
+        String imgUid = "";  //이미지 UID 가져오기
+        String content = ((EditText) findViewById(R.id.content)).getText().toString();  //문구
+//        Integer likeCount = 0;  //좋아요 수
+
+        Map<String, String> post = new HashMap<>();
+        post.put("userUid", userUid);
+        post.put("imgUid",imgUid);
+        post.put("content", content);
+
+
+        db.collection("cities").add(post)      //DB에 post 업로드
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {    //업로드 성공시
+                startToast("업로드 성공!");
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {     //업로드 실패시
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        startToast("업로드 실패!");
+                    }
+                });
+    }
+
+    private void startToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+
 }
