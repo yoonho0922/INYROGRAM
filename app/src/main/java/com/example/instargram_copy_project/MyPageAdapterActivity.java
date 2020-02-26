9e8a51912d71cc929f4d88ac4db3e59b758ccf59
limjustin <limjyjustin@naver.com>
2020-02-26 16:53:16 +0900

    mypage picture only read

---
+++ app/src/main/java/com/example/instargram_copy_project/MyPageAdapterActivity.java
@@ -0,0 +1,82 @@
+package com.example.instargram_copy_project;
+
+import androidx.annotation.NonNull;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MyPageAdapterActivity extends BaseAdapter {
+
+    private ArrayList<Map> item;
+
+    // ListViewAdapter의 생성자
+    public MyPageAdapterActivity(ArrayList item) {
+        this.item = item;
+    }
+
+    // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
+    @Override
+    public int getCount() {
+        return item.size() ;
+    }
+
+    // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Context context = parent.getContext();
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.mypage_custom_view, parent, false);
+        }
+
+        final ImageView test3 = convertView.findViewById(R.id.picture_posting);
+
+        final Map listViewItem = item.get(position);
+
+        // 아이템 내 각 위젯에 데이터 반영
+        if(listViewItem.get("fileName")!=null) {
+            FirebaseStorage.getInstance().getReference().child(listViewItem.get("fileName").toString()).getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    Glide.with(context).load(task.getResult()).into(test3);
+                }
+            });
+        }
+
+
+        return convertView;
+    }
+
+
+    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
+    @Override
+    public long getItemId(int position) {
+        return position ;
+    }
+
+    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
+    @Override
+    public Object getItem(int position) {
+        return item.get(position) ;
+    }
+
+}
+
